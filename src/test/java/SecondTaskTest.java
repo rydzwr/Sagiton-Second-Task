af8ee94924b7e90771e069b82f96f915f408fe49
rydzwr <rydzwr@gmail.com>
2022-08-19 00:51:29 +0200

    Added tests annotations

--- src/test/java/SecondTaskTest.java
+++ src/test/java/SecondTaskTest.java
@@ -1,4 +1,3 @@
-import com.rydzwr.secondtask.FooFilter;
 import com.rydzwr.secondtask.HelloServlet;
 
 import com.rydzwr.secondtask.ParamFilter;
@@ -8,48 +7,51 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import static org.mockito.Mockito.*;
 
-public class SecondTaskTest {
+public class SecondTaskTest
+{
     @Test
     @DisplayName("Check printed message")
-    public void testServlet() throws Exception {
+    public void testServlet() throws Exception
+    {
+        //GIVEN
         HttpServletRequest request = mock(HttpServletRequest.class);
         HttpServletResponse response = mock(HttpServletResponse.class);
-
         StringWriter stringWriter = new StringWriter();
         PrintWriter writer = new PrintWriter(stringWriter);
-        when(response.getWriter()).thenReturn(writer);
 
+        //WHEN
+        when(response.getWriter()).thenReturn(writer);
         new HelloServlet().doGet(request, response);
 
-        writer.flush(); // it may not have been flushed yet...
+        //THEN
         assertTrue(stringWriter.toString().contains("Hello"));
     }
 
     @Test
     @DisplayName("Check printed message with param value")
-    public void testServlet2() throws Exception {
+    public void testServlet2() throws Exception
+    {
+        //GIVEN
+        String foo = "Foo";
         HttpServletRequest request = mock(HttpServletRequest.class);
         HttpServletResponse response = mock(HttpServletResponse.class);
         FilterChain filterChain = mock(FilterChain.class);
-
-        when(request.getParameter(anyString())).thenReturn("Foo");
-
         StringWriter stringWriter = new StringWriter();
         PrintWriter writer = new PrintWriter(stringWriter);
-        when(response.getWriter()).thenReturn(writer);
 
+        //WHEN
+        when(request.getParameter(anyString())).thenReturn(foo);
+        when(response.getWriter()).thenReturn(writer);
         new ParamFilter().doFilter(request, response, filterChain);
 
-        writer.flush(); // it may not have been flushed yet...
-        assertTrue(stringWriter.toString().contains("Foo" + " " + "Hello World!" + " " + "Foo"));
+        //THEN
+        assertTrue(stringWriter.toString().contains(foo + " " + "Hello World!" + " " + foo));
     }
 }
